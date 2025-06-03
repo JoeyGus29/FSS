c74867b68bb311547b0fdd96867141ccb810213e
Joey G. <70541554+JoeyGus29@users.noreply.github.com>
2025-06-03 00:26:37 -0700

    Update 5_Player Comparison Page.py

--- pages/5_Player Comparison Page.py
+++ pages/5_Player Comparison Page.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.markdown("---")
 st.subheader("Compare Two Players")
-player1 = st.text_input(f"Player 1 ({pos})", "")
-player2 = st.text_input(f"Player 2 ({pos})", "")
+player1 = st.text_input("Player 1", "")
+player2 = st.text_input("Player 2", "")
 
 if player1 and player2:
     st.write(f"Comparison coming soon between **{player1}** and **{player2}**.")
